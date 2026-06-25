a4f3089ef447a90f708025b10b726087acf231bd
renato souza <renato.freitas@tecnico.ufcg.edu.br>
2026-06-25 16:59:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' //
 
-export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
-    base: '/agendamento-ccbs/', //
+export default defineConfig({
+  base: '/agendamento-ccbs/', //
+  plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
